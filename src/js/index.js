4328c0cc8008c609b5611cbd142b796f3346f99d
dylan edwards <dylanedwards@dylans-MacBook-Pro.local>
2021-11-05 16:59:17 -0700

    add new projects

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,13 @@
 import initTilt from "./tilt";
 import initSr from "./sr";
-import { addResume } from "./utils";
+import { addResume, addWhitepaper } from "./utils";
 import resume from "../assets/resume.pdf";
+import lyricGenerationWhitePaper from "../assets/lyricGenerationWhitePaper.pdf"
 
 export default function initApp() {
   initSr();
   initTilt();
   // comment this if you don't want to attach your resume
   addResume(resume);
+  addWhitepaper(lyricGenerationWhitePaper)
 }
--- src/js/utils.js
+++ src/js/utils.js
@@ -4,3 +4,12 @@ export function addResume(pdf) {
   const resumeButton = document.querySelector(".cta-btn--resume");
   resumeButton.setAttribute("href", pdf);
 }
+
+
+export function addWhitepaper(pdf) {
+  if (!pdf) return;
+
+  const whitePaperButton = document.querySelector(".cta-btn--whitepaper");
+  whitePaperButton.setAttribute("href", pdf);
+}
+
